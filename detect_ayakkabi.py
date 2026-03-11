7d34d869ecbaa064c4793cda373ca29f1ceb8ea5
Ayse Bingol <aysebingol@gmail.com>
2026-03-11 13:24:36 +0300

    box_result test

--- detect_ayakkabi.py
+++ detect_ayakkabi.py
@@ -1,12 +1,24 @@
 from ultralytics import YOLO
+import os
 
-model = YOLO('box_results/weights/last.pt') 
+# Yolları senin yapına göre netleştiriyoruz
+model_path = 'box_results/weights/best.pt'
+source_path = 'test/images'
 
-results = model.predict(
+if not os.path.exists(model_path):
+    print(f"Hata: {model_path} bulunamadı!")
+else:
+    model = YOLO(model_path)
+    
+   
+    results = model.predict(
     source='test/images', 
     save=True, 
-    conf=0.01,         
-    iou=0.01,        
-    agnostic_nms=True, 
+    conf=0.02,        
+    iou=0.1,        
+    agnostic_nms=True,
     device='cpu'
 )
+    
+    print(f"\n--- İşlem Tamam! ---")
+    print(f"Lütfen şu klasöre bakınız: box_result/deneme_1")
--- test/test_et.py
+++ test/test_et.py
@@ -1,8 +1,18 @@
 from ultralytics import YOLO
 import os
 
-model = YOLO('box_results/weights/best.pt')
+model_path = '../polygon_results/weights/best.pt'
 
-results = model.predict(source='test/images', save=True, conf=0.25)
-
-print("\n Test islemi bitti! Sonuclar 'runs/detect/predict' klasorune kaydedildi.")
+if not os.path.exists(model_path):
+    print(f"Hata: {model_path} bulunamadı! Lütfen dosya yolunu kontrol et.")
+else:
+    model = YOLO(model_path)
+    results = model.predict(
+        source='../test/images', 
+        save=True, 
+        conf=0.10, 
+        device='cpu',
+        project='../POLYGON_SONUCLAR',
+        name='deneme_1'
+    )
+    print("\n--- İşlem Tamam! Sonuçlar 'POLYGON_SONUCLAR' klasöründe. ---")
